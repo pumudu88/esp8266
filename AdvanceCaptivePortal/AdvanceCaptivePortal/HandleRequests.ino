4f73ad8936f291202d0a1a1229d34b67ccc9d98f
Pumudu Ruhunage <pumudu88@gmail.com>
2019-03-20 08:56:40 +0530

    Refactor project structure

---
+++ AdvanceCaptivePortal/AdvanceCaptivePortal/HandleRequests.ino
@@ -0,0 +1,91 @@
+void handleRoot(){
+  server.sendHeader("Location", "/index.html",true);   //Redirect to index html web page
+  server.send(302, "text/plane","");
+}
+
+bool handleFileRead(String path) {
+  Serial.println("inside handle file read");
+  Serial.println(path);
+
+  if(path.endsWith("connecttest.txt")) {
+    path = "/index.html";  
+  }
+
+  if(path.endsWith("redirect")) {
+    path = "/index.html";  
+  }
+  
+  if (path.endsWith("/")) {
+    path = "/index.html";
+  }
+
+  if (path.endsWith("generate_204") || path.endsWith("favicon.ico")) {
+    path = "/index.html";
+  }
+    
+  String contentType = getContentType(path, server.hasArg("download"));
+  
+  if (SPIFFS.exists(path)) {
+    Serial.println("index file exist!!");
+    File file = SPIFFS.open(path, "r");
+    server.streamFile(file, contentType);
+    file.close();
+    return true;
+  }
+  return false;
+}
+
+void handleSave() {
+  String str = "Done ...\r\n";
+
+  Serial.print("number of arguments "); 
+  Serial.println(server.args());                    // number of arguments
+
+
+  if (server.args() > 0 ) {
+    File f;
+    if(SPIFFS.exists("/savedata.txt")) {
+      f = SPIFFS.open("/savedata.txt", "a");
+    } else {
+      f = SPIFFS.open("/savedata.txt", "w");
+    }
+    
+    for ( uint8_t i = 0; i < server.args(); i++ ) {
+      str += server.argName(i) + " = " + server.arg(i) + "\r\n";
+      f.println(server.argName(i) + " = " + server.arg(i));
+
+      Serial.println("Arg "+ String(i)+"="+ server.argName(i));     
+      Serial.println("Arg "+ String(i)+"="+ server.arg(i));
+    }
+    f.close();
+  }
+  
+
+  str += "┌∩┐(◣_◢)┌∩┐          ┌∩┐(◣_◢)┌∩┐          ┌∩┐(◣_◢)┌∩┐\r\n";
+  str += "\r\n";
+  str += "┌∩┐(◣_◢)┌∩┐          ┌∩┐(◣_◢)┌∩┐          ┌∩┐(◣_◢)┌∩┐\r\n";
+  str += "\r\n";
+  str += "┌∩┐(◣_◢)┌∩┐          ┌∩┐(◣_◢)┌∩┐          ┌∩┐(◣_◢)┌∩┐\r\n";  
+  server.send(200, "text/plain", str.c_str());
+}
+
+
+void handleDownload() {
+  Serial.print("Download Page");   
+  server.send(200, "text/html", DOWNLOAD_HTML);
+}
+
+void handleRemove() {
+  Serial.print("Remove file operation");
+  String removeFile = "/savedata.txt";
+
+  if(SPIFFS.exists(removeFile)) {
+        if(SPIFFS.remove(removeFile)) {
+              server.send(200, "text/html", "Save file removed");          
+        } else {
+              server.send(200, "text/html", "Failed to remove file !!");
+        };
+  } else {
+    server.send(200, "text/html", " savedata txt file does not exist !!");  
+  }
+}
---
+++ AdvanceCaptivePortal/AdvanceCaptivePortal/Helpers.h
@@ -0,0 +1,41 @@
+#ifndef HELPERS_H
+#define HELPERS_H
+
+#include <Arduino.h>
+     
+static const char PROGMEM DOWNLOAD_HTML[] = R"rawliteral(  
+<!DOCTYPE html>
+<html>
+   <head>
+      <style>
+         table, th, td {
+         border: 1px solid black;
+         }
+      </style>
+   </head>
+   <body>
+      <h2>Settings Page</h2>
+      <table style="width:20%">
+         <tr>
+            <td align="center">
+               <form method="get" action="/savedata.txt">
+                  <button type="submit">View Save Data</button>
+               </form>
+            </td>
+         </tr>
+         <tr>
+            <td align="center">
+               <form method="get" action="/remove">
+                  <button type="submit">Clear</button>
+               </form>
+            </td>
+         </tr>
+      </table>
+   </body>
+</html>
+)rawliteral";
+
+String getContentType(String filename, boolean isDownload);
+String formatBytes(size_t bytes);
+
+#endif
